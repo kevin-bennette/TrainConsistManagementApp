3c47d34dfcff42037b8089e3995abf4d62f61bcc
kevin-bennette <kevinbennete006@gmail.com>
2026-03-30 09:36:44 +0530

    UC9: Group Bogies by Type

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 class Bogie {
@@ -22,22 +21,27 @@ public class TrainConsistManagementApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        System.out.println("\n--- UC8: Filter Passenger Bogies Using Streams ---");
+        System.out.println("\n--- UC9: Group Bogies by Type ---");
 
         List<Bogie> bogies = new ArrayList<>();
 
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("Sleeper", 72));       // duplicate type
         bogies.add(new Bogie("First Class", 40));
+        bogies.add(new Bogie("AC Chair", 60));      // duplicate type
 
-        List<Bogie> filteredBogies = bogies.stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
+        Map<String, List<Bogie>> groupedBogies =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(b -> b.name));
 
-        System.out.println("Filtered Bogies (capacity > 60):");
+        System.out.println("Grouped Bogies:");
 
-        for (Bogie b : filteredBogies) {
-            System.out.println(b);
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println("\n" + entry.getKey() + ":");
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b);
+            }
         }
     }
 }
