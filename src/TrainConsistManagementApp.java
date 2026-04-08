0ef24a30a01c342f2c3a92144d00ae001695318c
kevin-bennette <kevinbennete006@gmail.com>
2026-04-08 08:59:54 +0530

    UC18 - Linear Search for Bogie ID

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,35 +1,45 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TrainConsistManagementApp {
 
-    public static void sortBogieNames(String[] bogieNames) {
-        Arrays.sort(bogieNames);
+    public static boolean searchBogie(String[] bogieIds, String searchKey) {
+
+        for (int i = 0; i < bogieIds.length; i++) {
+
+            if (bogieIds[i].equals(searchKey)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("==== Train Consist Management App ====");
+        System.out.println("=== Train Consist Management App ===");
         System.out.print("Enter number of bogies: ");
         int n = scanner.nextInt();
         scanner.nextLine();
 
-        String[] bogieNames = new String[n];
+        String[] bogieIds = new String[n];
 
-        System.out.println("Enter bogie names:");
+        System.out.println("Enter bogie IDs:");
         for (int i = 0; i < n; i++) {
-            bogieNames[i] = scanner.nextLine();
+            bogieIds[i] = scanner.nextLine();
         }
 
-        System.out.println("\nBefore Sorting:");
-        System.out.println(Arrays.toString(bogieNames));
+        System.out.print("\nEnter bogie ID to search: ");
+        String searchKey = scanner.nextLine();
 
-        sortBogieNames(bogieNames);
+        boolean found = searchBogie(bogieIds, searchKey);
 
-        System.out.println("\nAfter Sorting (Alphabetical Order):");
-        System.out.println(Arrays.toString(bogieNames));
+        if (found) {
+            System.out.println("Bogie ID " + searchKey + " found in the train consist.");
+        } else {
+            System.out.println("Bogie ID " + searchKey + " NOT found.");
+        }
 
         scanner.close();
     }
